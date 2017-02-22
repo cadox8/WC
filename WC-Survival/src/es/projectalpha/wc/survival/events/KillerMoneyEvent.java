1ab5e39db1efd49a4805ac9e5192a5e6c1645e57
Wikijito7 <wikyfg@gmail.com>
2017-02-22 19:04:24 +0100

    Survival: Añadidos mobs y su dinero en el evento KME

--- WC-Survival/src/es/projectalpha/wc/survival/events/KillerMoneyEvent.java
+++ WC-Survival/src/es/projectalpha/wc/survival/events/KillerMoneyEvent.java
@@ -29,8 +29,74 @@ public class KillerMoneyEvent implements Listener {
             case COW:
                 eco.depositPlayer(p, 2);
                 break;
-
+            case CHICKEN:
+                eco.depositPlayer(p, 2);
+                break;
+            case PIG:
+                eco.depositPlayer(p, 2);
+                break;
+            case RABBIT:
+                eco.depositPlayer(p, 2);
+                break;
+            case ZOMBIE:
+                eco.depositPlayer(p, 2);
+                break;
+            case ZOMBIE_VILLAGER:
+                eco.depositPlayer(p, 2);
+                break;
+            case SKELETON:
+                eco.depositPlayer(p, 2);
+                break;
+            case WITHER_SKELETON:
+                eco.depositPlayer(p, 2);
+                break;
+            case SPIDER:
+                eco.depositPlayer(p, 2);
+                break;
+            case CAVE_SPIDER:
+                eco.depositPlayer(p, 2);
+                break;
+            case HORSE:
+                eco.depositPlayer(p, 2);
+                break;
+            case SKELETON_HORSE:
+                eco.depositPlayer(p, 2);
+                break;
+            case ZOMBIE_HORSE:
+                eco.depositPlayer(p, 2);
+                break;
+            case PIG_ZOMBIE:
+                eco.depositPlayer(p, 2);
+                break;
+            case VILLAGER:
+                eco.withdrawPlayer(p, 500);
+                break;
+            case SILVERFISH:
+                eco.depositPlayer(p, 2);
+                break;
+            case IRON_GOLEM:
+                eco.withdrawPlayer(p, 500);
+                break;
+            case SNOWMAN:
+                eco.withdrawPlayer(p, 500);
+                break;
+            case OCELOT:
+                eco.depositPlayer(p, 2);
+                break;
+            case ENDER_DRAGON:
+                eco.depositPlayer(p, 500);
+                break;
+            case WITHER:
+                eco.depositPlayer(p, 250);
+                break;
+            case WITCH:
+                eco.depositPlayer(p, 2);
+                break;
+            case SLIME:
+                eco.depositPlayer(p, 2);
+                break;
         }
+
 
     }
 
